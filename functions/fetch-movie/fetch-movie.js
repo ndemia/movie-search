d4f6c392ac908fd58272eaf27e9098f1b7ce8a50
ndemia <info@demia.me>
2023-01-10 12:03:28 +0100

    Remove commented code

--- functions/fetch-movie/fetch-movie.js
+++ functions/fetch-movie/fetch-movie.js
@@ -1,18 +1,6 @@
 const fetch = require('node-fetch');
 
 const handler = async (event) => {
-  // try {
-  //   const subject = event.queryStringParameters.name || 'World'
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({ message: `Hello ${subject}` }),
-  //     // // more keys you can return:
-  //     // headers: { "headerName": "headerValue", ... },
-  //     // isBase64Encoded: true,
-  //   }
-  // } catch (error) {
-  //   return { statusCode: 500, body: error.toString() }
-  // }
 
   let movieName = event.queryStringParameters.s;
   const API_KEY = process.env.API_KEY;
