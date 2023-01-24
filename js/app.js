6cfe5f68ee30c3e943679d47d4cb33be2c1167ae
ndemia <info@demia.me>
2023-01-24 19:30:14 +0100

    Add button functions

--- js/app.js
+++ js/app.js
@@ -3,17 +3,45 @@ async function fetchMovie (movieName) {
   const response = await fetch(`/.netlify/functions/fetch-movie?s=${movieName}`);
   const movieSearchResults = await response.json();
   return movieSearchResults;
+
+};
+
+
+
+function disableButton (e) {
+
+  e.target.classList.toggle('btn--disabled');
+
+  // Remove focus and active states that remain after being clicked
+  e.target.blur();
+
 }
 
-document.querySelector('.form__submit').addEventListener('click', (e) => {
 
+  
+function enableButton (e) {
+  e.target.classList.toggle('btn--disabled');
+}
+
+
+
+document.querySelector('.btn--submit').addEventListener('click', (e) => {
+
+  // Prevent the form from submitting, which could create issues
   e.preventDefault();
 
   let movieName = document.querySelector('.form__input').value;
 
-  fetchMovie(movieName)
-    .then((results) => {console.log(results)})
-    .catch((error) => console.log(error));
+  disableButton(e);
+
+  // Simulate delay for illustration purposes
+  setTimeout(() => {
+
+    fetchMovie(movieName)
+      .then((results) => {console.log(results)})
+      .catch((error) => console.log(error));
+
+  }, 3000);
 
   // fetch(`/.netlify/functions/fetch-movie?s=${movieName}`)
   //   .then((response) => response.json())
