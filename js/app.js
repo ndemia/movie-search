00b64cb893807d3d97a4ec8a76e76c995f0abbf3
ndemia <info@demia.me>
2023-01-25 11:29:06 +0100

    Add loader and its functionality

--- js/app.js
+++ js/app.js
@@ -10,9 +10,11 @@ async function fetchMovie (movieName) {
 
 function disableButton (e) {
 
+  e.target.setAttribute('disabled', true);
+
   e.target.classList.toggle('btn--disabled');
 
-  // Remove focus and active states that remain after being clicked
+  // Remove focus and active states that remain after the button is clicked
   e.target.blur();
 
 }
@@ -25,14 +27,23 @@ function enableButton (e) {
 
 
 
+
+function showLoader () {
+  document.querySelector('.loader').classList.toggle('hidden');
+}
+
+
+
 document.querySelector('.btn--submit').addEventListener('click', (e) => {
 
-  // Prevent the form from submitting, which could create issues
+  // Prevent the form from submitting, which creates issues when it's not the actual intention
   e.preventDefault();
 
   let movieName = document.querySelector('.form__input').value;
 
   disableButton(e);
+
+  showLoader();
 
   // Simulate delay for illustration purposes
   setTimeout(() => {
@@ -42,10 +53,5 @@ document.querySelector('.btn--submit').addEventListener('click', (e) => {
       .catch((error) => console.log(error));
 
   }, 3000);
-
-  // fetch(`/.netlify/functions/fetch-movie?s=${movieName}`)
-  //   .then((response) => response.json())
-  //   .then((content) => console.log(content.Search[0]))
-  //   .catch((error) => console.log(error.message));
 
 });
